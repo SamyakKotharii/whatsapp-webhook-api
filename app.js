b6af10781383a55848e784d2fc87079eefcbaba6
SamyakKotharii <shkothari904.sk@gmail.com>
2023-05-28 12:06:18 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-// /*
-//  * Starter Project for WhatsApp Echo Bot Tutorial
-//  *
-//  * Remix this as the starting point for following the WhatsApp Echo Bot tutorial
-//  *
-//  */
+/*
+ * Starter Project for WhatsApp Echo Bot Tutorial
+ *
+ * Remix this as the starting point for following the WhatsApp Echo Bot tutorial
+ *
+ */
 
-// "use strict";
+"use strict";
 
-// // Access token for your app
-// // (copy token from DevX getting started page
-// // and save it as environment variable into the .env file)
-// const token = process.env.TEMPORARY_ACCESS_TOKEN;
+// Access token for your app
+// (copy token from DevX getting started page
+// and save it as environment variable into the .env file)
+const token = process.env.TEMPORARY_ACCESS_TOKEN;
 
-// // Imports dependencies and set up http server
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const axios = require("axios").default;
-// const cors = require("cors"); // Import cors middleware
-// const app = express().use(bodyParser.json()); // creates express http server
+// Imports dependencies and set up http server
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios").default;
+const cors = require("cors"); // Import cors middleware
+const app = express().use(bodyParser.json()); // creates express http server
 
-// // Enable CORS
-// app.use(cors());
-// // Sets server port and logs message on success
-// app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+// Enable CORS
+app.use(cors());
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
-// // Accepts POST requests at /webhook endpoint
+// Accepts POST requests at /webhook endpoint
 // app.post("/webhook", (req, res) => {
 //   // Parse the request body from the POST
 //   let body = req.body;
@@ -72,35 +72,7 @@
 //     res.sendStatus(404);
 //   }
 // });
-// // Accepts POST requests at /webhook endpoint
-
-// // Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
-// // info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
-// app.get("/webhook", (req, res) => {
-//   /**
-//    * UPDATE YOUR VERIFY TOKEN
-//    *This will be the Verify Token value when you set up webhook
-//    **/
-//   const verify_token = process.env.VERIFY_TOKEN;
-
-//   // Parse params from the webhook verification request
-//   let mode = req.query["hub.mode"];
-//   let token = req.query["hub.verify_token"];
-//   let challenge = req.query["hub.challenge"];
-
-//   // Check if a token and mode were sent
-//   if (mode && token) {
-//     // Check the mode and token sent are correct
-//     if (mode === "subscribe" && token === verify_token) {
-//       // Respond with 200 OK and challenge token from the request
-//       console.log("WEBHOOK_VERIFIED");
-//       res.status(200).send(challenge);
-//     } else {
-//       // Responds with '403 Forbidden' if verify tokens do not match
-//       res.sendStatus(403);
-//     }
-//   }
-// });
+// Accepts POST requests at /webhook endpoint
 // ...
 
 // Accepts POST requests at /webhook endpoint
@@ -159,3 +131,31 @@ app.post("/webhook", (req, res) => {
 });
 
 // ...
+
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+  /**
+   * UPDATE YOUR VERIFY TOKEN
+   *This will be the Verify Token value when you set up webhook
+   **/
+  const verify_token = process.env.VERIFY_TOKEN;
+
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
