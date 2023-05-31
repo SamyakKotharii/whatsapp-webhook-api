b07fbb90e1f37f961496f89598ddb67fff08d826
SamyakKotharii <shkothari904.sk@gmail.com>
2023-05-31 16:23:32 +0530

    Modified

--- app.js
+++ app.js
@@ -1,31 +1,67 @@
-/*
- * Starter Project for WhatsApp Echo Bot Tutorial
- *
- * Remix this as the starting point for following the WhatsApp Echo Bot tutorial
- *
- */
-
 "use strict";
 
-// Access token for your app
-// (copy token from DevX getting started page
-// and save it as environment variable into the .env file)
-const token = process.env.TEMPORARY_ACCESS_TOKEN;
-
-// Imports dependencies and set up http server
+require("dotenv").config(); // Load environment variables
+require("./db");
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios").default;
-const cors = require("cors"); // Import cors middleware
-const app = express().use(bodyParser.json()); // creates express http server
+const cors = require("cors");
 
-// Enable CORS
+const app = express().use(bodyParser.json());
 app.use(cors());
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
-// Accepts POST requests at /webhook endpoint
-app.post("/webhook", (req, res) => {
+app.listen(process.env.PORT || 1337, () => {
+  console.log("Webhook is listening");
+});
+// app.post("/webhook", (req, res) => {
+//   // Parse the request body from the POST
+//   let body = req.body;
+
+//   // Check the Incoming webhook message
+//   console.log(JSON.stringify(req.body, null, 2));
+
+//   // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+//   if (req.body.object) {
+//     if (
+//       req.body.entry &&
+//       req.body.entry[0].changes &&
+//       req.body.entry[0].changes[0] &&
+//       req.body.entry[0].changes[0].value.messages &&
+//       req.body.entry[0].changes[0].value.messages[0]
+//     ) {
+//       let phone_number_id =
+//         req.body.entry[0].changes[0].value.metadata.phone_number_id;
+//       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+//       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+//       axios({
+//         method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+//         url:
+//           "https://graph.facebook.com/v12.0/" +
+//           phone_number_id +
+//           "/messages?access_token=" +
+//           token,
+//         data: {
+//           messaging_product: "whatsapp",
+//           to: from,
+//           text: { body: "Ack: " + msg_body },
+//         },
+//         headers: { "Content-Type": "application/json" },
+//       })
+//         .then((response) => {
+//           console.log("Response:", response.data);
+//           res.status(200).send(response.data);
+//         })
+//         .catch((error) => {
+//           console.error("An error occurred:", error);
+//         });
+//     }
+//     res.sendStatus(200);
+//   } else {
+//     // Return a '404 Not Found' if event is not from a WhatsApp API
+//     res.sendStatus(404);
+//   }
+// });
+app.post("/webhook", async (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
@@ -45,29 +81,35 @@ app.post("/webhook", (req, res) => {
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-      axios({
-        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-        url:
-          "https://graph.facebook.com/v12.0/" +
-          phone_number_id +
-          "/messages?access_token=" +
-          token,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: { body: "Ack: " + msg_body },
-        },
-        headers: { "Content-Type": "application/json" },
-      })
-        .then((response) => {
-          console.log("Response:", response.data);
-          res.status(200).send(response.data);
-        })
-        .catch((error) => {
-          console.error("An error occurred:", error);
+
+      try {
+        // Create a new message object
+        const message = new Message({
+          from: from,
+          timestamp: new Date(),
+          text: [{ text: msg_body }],
         });
+
+        // Save the message to the database
+        await message.save();
+
+        // Send acknowledgment message back to the sender
+        await axios.post(
+          `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${token}`,
+          {
+            messaging_product: "whatsapp",
+            to: from,
+            text: { body: "Ack: " + msg_body },
+          }
+        );
+
+        console.log("Message saved and acknowledgment sent");
+        res.sendStatus(200);
+      } catch (error) {
+        console.error("An error occurred:", error);
+        res.sendStatus(500);
+      }
     }
-    res.sendStatus(200);
   } else {
     // Return a '404 Not Found' if event is not from a WhatsApp API
     res.sendStatus(404);
@@ -80,7 +122,7 @@ app.get("/webhook", (req, res) => {
   /**
    * UPDATE YOUR VERIFY TOKEN
    *This will be the Verify Token value when you set up webhook
-   **/
+   **/ z;
   const verify_token = process.env.VERIFY_TOKEN;
 
   // Parse params from the webhook verification request
---
+++ models/MessageModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  from: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+  },
+  text: [
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
