e95510d0d01cb3d9de471b81c2e2dc2a364e2a92
SamyakKotharii <shkothari904.sk@gmail.com>
2023-06-13 10:14:27 +0530

    Modified

--- app.js
+++ app.js
@@ -112,7 +112,9 @@ app.post("/webhook", async (req, res) => {
       let phone_number_id =
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      let msg_body =
+        req.body?.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.text?.body;
+      // extract the message text from the webhook payload
 
       try {
         // Find an existing message document by 'from' field
