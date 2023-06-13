b2d0265ffaddb96bf5c9101b535254559818323a
SamyakKotharii <shkothari904.sk@gmail.com>
2023-06-13 11:03:59 +0530

    Modified

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -1,5 +1,31 @@
 const mongoose = require("mongoose");
 
+// const messageSchema = new mongoose.Schema({
+//   from: {
+//     type: String,
+//     required: true,
+//   },
+//   timestamp: {
+//     type: Date,
+//     required: true,
+//   },
+//   text: [
+//     {
+//       text: {
+//         type: String,
+//         required: true,
+//       },
+//       timestamp: {
+//         type: Date,
+//         required: true,
+//       },
+//       role: {
+//         type: Number,
+//         default: 0,
+//       },
+//     },
+//   ],
+// });
 const messageSchema = new mongoose.Schema({
   from: {
     type: String,
