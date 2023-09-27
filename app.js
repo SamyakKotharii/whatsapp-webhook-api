b0114f8114c536064d13b5439995bd16f9e7a131
SamyakKotharii <shkothari904.sk@gmail.com>
2023-09-27 13:06:25 +0530

    Modified

--- app.js
+++ app.js
@@ -125,6 +125,7 @@ app.post("/send-message", async (req, res) => {
       wabaNumber: "917874990975",
       recipient: to,
       source: "",
+      clientRefId: "",
       type: "text",
       text: {
         preview_url: false,
